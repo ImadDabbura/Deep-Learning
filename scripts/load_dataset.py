3b4ca89cf62c881999567ada5110b3881e6fad25
Imad Dabbura <imad.dabbura@hotmail.com>
2017-10-19 09:14:54 -0500

    Changed the doc string to reflect that it will be applied on ".jpg" images. Changed the functionality to only import jpg images.

--- scripts/load_dataset.py
+++ scripts/load_dataset.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def load_dataset(path):
+def load_dataset_catvsdog(path):
     """
-    Loads dataset as numpy objects. The data will be shuffled to avoid
-    pre-existing ordering.
+    Loads dataset as numpy objects. It will import only images with ".jpg"
+    extension.
+    The data will be shuffled to avoid pre-existing ordering.
 
     Arguments:
     path: path of the file from current directory.
@@ -20,20 +21,21 @@ def load_dataset(path):
     os.chdir(path)
 
     # get all file names to iterate over all of them
-    image_list_names = os.listdir()[1:]
+    image_list_names = os.listdir()
     m = len(image_list_names)
 
     # loading images
     X = []
 
-    for img in image_list_names:
-        if img.startswith("."):
+    temp_list = image_list_names[:]
+    for img in temp_list:
+        if not img.endswith(".jpg"):
             m -= 1
-        
+            image_list_names.remove(img)
         else:
             temp = np.array(plt.imread(img))
             X.append(temp)
-            
+
     # convert to numpy array
     X = np.array(X)
 
@@ -46,7 +48,7 @@ def load_dataset(path):
     for i, img in enumerate(image_list_names):
         if img.startswith("cat"):
             Y[:, i] = 1
-            
+
         elif img.startswith("dog"):
             Y[:, i] = 0
 

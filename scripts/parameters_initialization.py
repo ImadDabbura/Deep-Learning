30ac9b9d2948e5fb0af49bc8a92865791c406449
Imad <imad.dabbura@hotmail.com>
2018-04-09 11:07:43 -0500

    Follow numpy style in parameters initialization script

--- scripts/parameters_initialization.py
+++ scripts/parameters_initialization.py
@@ -1,8 +1,11 @@
+"""
+Implementing three different parameters initialization methods.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Importing helper functions from local module
-from coding_deep_neural_network_from_scratch import (L_model_forward,
+from .coding_neural_network_from_scratch import (L_model_forward,
                                                      compute_cost,
                                                      L_model_backward,
                                                      update_parameters,
@@ -14,15 +17,19 @@ def initialize_parameters_zeros(layers_dims):
     Initializes the parameters dictionary to all zeros for both weights and
     bias.
 
-    Arguments:
-    layer_dims -- python array (list) containing the size of each layer.
+    Arguments
+    ---------
+    layer_dims : list
+        input size and size of each layer, length: number of layers + 1.
 
-    Returns:
-    parameters -- python dictionary containing all parameters Wl, bl.
+    Returns
+    -------
+    parameters : dict
+        weight matrix and the bias vector for each layer.
     """
-    np.random.seed(1)               # to get consistent output
-    parameters = {}                 # initialize parameters dictionary
-    L = len(layers_dims)            # number of layers in the network
+    np.random.seed(1)               
+    parameters = {}                 
+    L = len(layers_dims)            
 
     for l in range(1, L):
         parameters["W" + str(l)] = np.zeros(
@@ -38,15 +45,19 @@ def initialize_parameters_random(layers_dims):
     distribution multiplied by 10 for weight matrices and zeros for bias
     vectors.
 
-    Arguments:
-    layer_dims -- python array (list) containing the size of each layer.
+    Arguments
+    ---------
+    layer_dims : list
+        input size and size of each layer, length: number of layers + 1.
 
-    Returns:
-    parameters -- python dictionary containing all parameters Wl, bl.
+    Returns
+    -------
+    parameters : dict
+        weight matrix and the bias vector for each layer.
     """
-    np.random.seed(1)               # to get consistent output
-    parameters = {}                 # initialize parameters dictionary
-    L = len(layers_dims)            # number of layers in the network
+    np.random.seed(1)               
+    parameters = {}                 
+    L = len(layers_dims)            
 
     for l in range(1, L):
         parameters["W" + str(l)] = np.random.randn(
@@ -61,17 +72,21 @@ def initialize_parameters_he_xavier(layers_dims, initialization_method="he"):
     Initializes the parameters dictionary for weights based on "He" and
     "Xavier" methods and zeros for bias vectors.
 
-    Arguments:
-    layer_dims -- python array (list) containing the size of each layer.
-    initialization_method -- string specify the initialization method to be
-                             used: "he", "xavier".
+    Arguments
+    ---------
+    layer_dims : list
+        input size and size of each layer, length: number of layers + 1.
+    initialization_method : str
+        specify the initialization method to be used: "he", "xavier".
 
-    Returns:
-    parameters -- python dictionary containing all parameters Wl, bl
+    Returns
+    -------
+    parameters : dict
+        weight matrix and the bias vector for each layer.
     """
-    np.random.seed(1)               # to get consistent output
-    parameters = {}                 # initialize parameters dictionary
-    L = len(layers_dims)            # number of layers in the network
+    np.random.seed(1)               
+    parameters = {}                 
+    L = len(layers_dims)            
 
     if initialization_method == "he":
         for l in range(1, L):
@@ -96,22 +111,31 @@ def model(X, Y, layers_dims, learning_rate=0.01, num_iterations=1000,
     Implements multilayer neural network using gradient descent as the
     learning algorithm.
 
-    Arguments:
-    X -- input data, shape: num_px * num_px * 3 x number of examples.
-    Y -- true "label" vector, shape: 1 x number of examples.
-    layers_dims -- list containing the size and each layer.
-    learning_rate -- learning rate of the gradient descent update rule
-    num_iterations -- number of iterations of the optimization loop
-    print_cost -- if True, it prints the cost every 100 steps
-    hidden_layers_activation_fn -- activation function to be used on hidden
-                                   layers, string: "tanh", "relu"
-    initialization_method -- flag to choose which initialization to use
-                             ("zeros","random", "he", or "xavier")
+    Arguments
+    ---------
+    X : 2d-array
+        data, shape: number of examples x num_px * num_px * 3.
+    y : 2d-array
+        true "label" vector, shape: 1 x number of examples.
+    layers_dims : list
+        input size and size of each layer, length: number of layers + 1.
+    learning_rate : float
+        learning rate of the gradient descent update rule.
+    num_iterations : int
+        number of iterations of the optimization loop.
+    print_cost : bool
+        if True, it prints the cost every 100 steps.
+    hidden_layers_activation_fn : str
+        activation function to be used on hidden layers: "tanh", "relu".
+    initialization_method : str
+        specify the initialization method to be used: "he", "xavier".
 
-    Returns:
-    parameters -- parameters learnt by the model.
+    Returns
+    -------
+    parameters : dict
+        parameters learnt by the model. They can then be used to predict test
+        examples.
     """
-    # to get consistent results
     np.random.seed(1)
 
     # initialize cost list
@@ -120,10 +144,8 @@ def model(X, Y, layers_dims, learning_rate=0.01, num_iterations=1000,
     # initialize parameters
     if initialization_method == "zeros":
         parameters = initialize_parameters_zeros(layers_dims)
-
     elif initialization_method == "random":
         parameters = initialize_parameters_random(layers_dims)
-
     else:
         parameters = initialize_parameters_he_xavier(
             layers_dims, initialization_method)
